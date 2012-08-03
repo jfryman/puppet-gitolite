221f93eb459b941febf9a937e22a2a9655195cbe
Hunter Haugen <h.haugen@gmail.com>
2012-08-03 15:26:13 -0700

    Don't create the repo.git bare directory, because this will stop gitolite from creating the repository when pushed

--- manifests/hook.pp
+++ manifests/hook.pp
@@ -1,18 +1,11 @@
 define gitolite::hook {
   $repo = $name
 
-  File {
-    owner => $gitolite::params::gt_uid,
-    group => $gitolite::params::gt_gid,
-  }
-
-  file { "${gitolite::params::gt_repo_dir}/${repo}.git":
-    ensure => directory,
-    mode   => '0755',
-  }
   file { "${gitolite::params::gt_repo_dir}/${repo}.git/hooks":
     ensure  => directory,
     recurse => true,
+    owner   => $gitolite::params::gt_uid,
+    group   => $gitolite::params::gt_gid,
     source  => "puppet:///modules/${gitolite::params::gt_hooks_module}/${repo}",
     require => Class['gitolite::server']
   }
